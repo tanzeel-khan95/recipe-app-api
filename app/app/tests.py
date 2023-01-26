706b4a7ccb7f5a4c66a08f92a38b4939228f740a
tanzeel-khan95 <tanzeel.khan42@gmail.com>
2023-01-26 12:49:54 +0500

    flake8 formatting

--- app/app/tests.py
+++ app/app/tests.py
@@ -12,7 +12,6 @@ class CalcTests(SimpleTestCase):
     def test_add_numbers(self):
         """Test adding numbers together."""
         res = calc.add(5, 6)
-
         self.assertEqual(res, 11)
 
     def test_subtract_numbers(self):
@@ -22,8 +21,9 @@ class CalcTests(SimpleTestCase):
         self.assertEqual(res, 5)
 
 
-'''We can use Mock testing to test functions depending on some external services like requesting data from external 
-APIs steps are as follows:
+'''We can use Mock testing to test functions depending on
+some external services like requesting data from external APIs
+steps are as follows:
     -import APIClient
     -Create client
     -make request
